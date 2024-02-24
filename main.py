7d7be03905277c91349f7e467d9e9ea0757cd1bf
AddisonReyes <106483642+AddisonReyes@users.noreply.github.com>
2024-02-24 11:45:56 -0400

    remove task function

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ class to_do_list():
         self.tasks = np.array([])
     
     def add_task(self, new_task):
+        if new_task == '-1':
+            return
+        
         self.tasks = np.append(self.tasks, [new_task], axis=0)
 
         self.df = pd.DataFrame(self.tasks)
@@ -26,9 +29,19 @@ class to_do_list():
         print("task added successfully")
 
     def remove_task(self, idx):
-        pass
+        if idx == -1:
+            return
+        
+        self.df.drop(index=idx, inplace=True)
+        print("Task deleted successfully")
 
     def change_task(self, idx):
+        pass
+
+    def import_list(self):
+        pass
+
+    def export_liste(self):
         pass
 
     def print_tasks(self):
@@ -50,12 +63,18 @@ def main():
         option = str(input("Choose a option: ")).strip()
 
         if option == 'add task' or option == 'add':
-            my_list.add_task(str(input("Type the new task: ")))
+            my_list.add_task(str(input("Type the new task, -1 to cancel: ")))
         
-        elif option == 'remove task':
-            pass
+        elif option == 'remove task' or option == 'remove':
+            my_list.print_tasks()
+
+            try:
+                my_list.remove_task(int(input("\nType the idx to remove, -1 to cancel: ")))
+            
+            except:
+                print("Error: no index provided")
         
-        elif option == 'tasks':
+        elif option == 'tasks' or option == 'task':
             my_list.print_tasks()
         
         elif option == 'exit':
